7f60c5e54a0ae1ffbeafdb076b9a99270ec3953f
Dev <p.trojnacki@o2.pl>
2019-11-04 20:21:01 +0100

    ok

---
+++ src/pages/kontakt.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Layout from '../components/Layout';
+
+const contact = () => {
+    return (
+        <Layout>
+            <div>Skontaktuj się z nami</div>
+        </Layout>
+    )
+}
+
+export default contact
