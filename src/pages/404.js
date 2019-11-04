d8ec3573f069f586300ec9acc3b7d0f6550f053c
Dev <p.trojnacki@o2.pl>
2019-11-04 20:12:48 +0100

    add 404

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,8 +1,11 @@
 import React from "react"
+import Layout from "../components/Layout"
 
 const page404 = () => {
   return (
-    <div>ok</div>
+    <Layout>
+        <div>Nie ma takiej strony</div>
+    </Layout>
 )
 }
 
