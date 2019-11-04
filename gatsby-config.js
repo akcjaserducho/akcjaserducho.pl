f50869358fd3b6caa7309cc9233095361d34dc3d
Dev <p.trojnacki@o2.pl>
2019-11-04 20:38:50 +0100

    change config

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Akcja Serducho`,
+    description: `Fundacja Akcja Serducho - osoby z wielkimi sercami podejmują sie pracy z młodzieżą i ich rodzinami, którzy w swoim życiu doświadczyli wielu trudności. Wierzymy, że ci, do których chcemy wyjść i o których pragniemy powalczyć mają w sobie ogromny potencjał, który jest jednak zasłonięty cierpieniem, wyuczoną bezradnością, poczuciem beznadziejności czy brakiem wiary w siebie.`,
+    author: `Trojnacki & Draganek`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
@@ -18,11 +18,12 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
+        name: `Akcja Serducho`,
+        short_name: `Akcja Serducho`,
         start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
+        lang: `pl`,
+        background_color: `#800000`,
+        theme_color: `#800000`,
         display: `minimal-ui`,
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
